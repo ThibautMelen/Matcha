6cc40994c5eadca012963157e5b6631c969eaf08
Thibaut Melen <thibautmelen@Thibauts-MacBook-Pro.local>
2019-07-30 17:08:33 +0200

    nique ta mere sql

---
+++ matchapi/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mysql = require('mysql');
+
+// Create connection
+const db = mysql.createConnection({
+    socketPath : '/tmp/mysql.sock',
+    port: 3306,
+    host     : '127.0.0.1',
+    user     : 'root',
+    password : '',
+    database : 'tamere',
+    insecureAuth: true
+});
+
+// Connect
+db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('MySql Connected...');
+});
+
+const app = express();
+
+app.listen('3000', () => {
+    console.log('Server started on port 3000');
+});
